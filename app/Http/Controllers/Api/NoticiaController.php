b1ae0905eaf1ec84665150277c3fc32513ff9b6a
Eduardo-Baranowski <eduardobaranowski@gmail.com>
2025-03-01 13:25:08 -0300

    feat: routes for noticias and authenticated

--- app/Http/Controllers/Api/NoticiaController.php
+++ app/Http/Controllers/Api/NoticiaController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\NoticiaRequest;
+use App\Http\Requests\UserRequest;
 use App\Models\Noticia;
+use App\Models\User;
 use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
@@ -12,6 +14,13 @@ use Illuminate\Support\Facades\DB;
 
 class NoticiaController extends Controller
 {
+    public function index() : JsonResponse{
+        $noticias = Noticia::orderBy('id', 'ASC')->where('id_user', Auth::user()->getAuthIdentifier())->paginate(10);
+        return response()->json([
+            'status' => true,
+            'noticias' => $noticias,
+        ], 200);
+    }
     public function store(NoticiaRequest $request) : JsonResponse{
         DB::beginTransaction();
 
@@ -36,6 +45,58 @@ class NoticiaController extends Controller
             return response()->json([
                 'status' => false,
                 'message' => 'Notícia não cadastrada!',
+            ], 400);
+        }
+    }
+
+    public function show(Noticia $noticia) : JsonResponse{
+        $noticias = User::orderBy('id', 'DESC')->paginate(2);
+        return response()->json([
+            'status' => true,
+            'noticia' => $noticia,
+        ], 200);
+    }
+
+    public function update(NoticiaRequest $request, Noticia $noticia) : JsonResponse{
+
+        DB::beginTransaction();
+
+        try{
+            $noticia->update([
+                'title' => $request->title,
+                'content_text' => $request->content_text,
+            ]);
+
+            DB::commit();
+
+            return response()->json([
+                'status' => true,
+                'noticia' => $noticia,
+                'message' => 'Notícia editada com sucesso!',
+            ], 200);
+
+        }catch(Exception $e){
+            DB::rollBack();
+            return response()->json([
+                'status' => false,
+                'message' => 'Notícia editada com sucesso!',
+            ], 400);
+        }
+    }
+
+    public function destroy(Noticia $noticia) : JsonResponse{
+        try{
+            $noticia->delete();
+
+            return response()->json([
+                'status' => true,
+                'noticia' => $noticia,
+                'message' => 'Notícia deletado com sucesso!',
+            ], 200);
+        }catch(Exception $e){
+            return response()->json([
+                'status' => false,
+                'message' => 'Notícia não apagado!',
             ], 400);
         }
     }
--- routes/api.php
+++ routes/api.php
@@ -5,16 +5,18 @@ use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\NoticiaController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/users/{user}', [UserController::class, 'show']);
-Route::post('/users', [UserController::class, 'store']);
-Route::put('/users/{user}', [UserController::class, 'update']);
-Route::delete('/users/{user}', [UserController::class, 'destroy']);
-
 Route::post('/login', [LoginController::class, 'login'])->name('login');
 
 Route::group(['middleware' => ['auth:sanctum']], function(){
+    Route::get('/users/{user}', [UserController::class, 'show']);
+    Route::post('/users', [UserController::class, 'store']);
+    Route::put('/users/{user}', [UserController::class, 'update']);
+    Route::delete('/users/{user}', [UserController::class, 'destroy']);
+    Route::put('/noticias/{noticia}', [NoticiaController::class, 'update']);
+    Route::delete('/noticias/{noticia}', [NoticiaController::class, 'destroy']);
+    Route::get('/noticias/{noticia}', [NoticiaController::class, 'show']);
     Route::get('/users', [UserController::class, 'index']);
+    Route::get('/noticias', [NoticiaController::class, 'index']);
     Route::post('/logout/{user}', [UserController::class, 'logout']);
     Route::post('/noticias', [NoticiaController::class, 'store']);
 });
